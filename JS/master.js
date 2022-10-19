520a4bf3dd9e60ed23e2a30fc91df3749905da9a
Angelo31294 <luongoangelo1@gmail.com>
2022-10-19 16:16:16 +0200

    Agg. i "prompt" del nome, cognome e colore

--- JS/master.js
+++ JS/master.js
@@ -1,4 +1,13 @@
 // Chiedi all'utente il suo nome
+const firtsName = prompt("Ciao, inserisci il tuo nome");
+console.log(typeof firtsName, firtsName);
+
 // Chiedi all'utente il suo cognome
+const lastName = prompt("Inserisci il tuo cognome");
+console.log(typeof lastName, lastName);
+
 // Chiedi all'utente il suo colore preferito
+const color = prompt("Inserisci il tuo colore preferito");
+console.log(typeof color, color);
+
 // Scrivi sulla pagina nomecognomecolorepreferito76
